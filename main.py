a365417cb622d0132b8ccca8105189b4a9d052c2
Relizene <vovaegorov689@gmail.com>
2025-10-07 16:39:01 +0300

    fetch: Asynchronic version with motor
    - add motor for MongoDb connetion
    - refactor all code for async function
    - add documentation: LICENSE, README

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from bson import ObjectId
-from fastapi import FastAPI, HTTPException
-from pymongo import MongoClient
-from pymongo.errors import PyMongoError
+from fastapi import FastAPI
 from pydantic import  BaseModel
 import uvicorn
-from pdf import pdf_formater, delete_file
+import motor.motor_asyncio
+import asyncio
 import os
-
+from pdf import pdf_formater, delete_file
 
 app = FastAPI()
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-client = MongoClient(MONGO_URL)
-db = client['book_library']
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+db = client.book_library
 books_collection = db.book
 books_pages = db.pages
 
@@ -28,10 +27,17 @@ async def upload_books(metadata: BookModels) :
     id_back = None
     try:     
         data_for_books_collection = {'book_name' : metadata.book_name,}
-        id_back = books_collection.insert_one(data_for_books_collection).inserted_id
-        books_content = pdf_formater(metadata.content, metadata.book_name, id_back)
+        insert_book_result = await books_collection.insert_one(data_for_books_collection)
+        id_back = insert_book_result.inserted_id
+        books_content = await asyncio.get_event_loop().run_in_executor(
+            None,
+            pdf_formater,
+            metadata.content,
+            metadata.book_name,
+            id_back
+        )
         if books_content:
-            insert_result = books_pages.insert_many(books_content)
+            insert_result = await books_pages.insert_many(books_content)
             return {
                 'message' : 'Книга сохранена', 
                 'сохраненные страницы' : f'{len(insert_result.inserted_ids)} ',
@@ -39,23 +45,24 @@ async def upload_books(metadata: BookModels) :
                 'cod' : '1001'
                     }
         else:
-            books_collection.delete_one({'_id' : id_back})
+            await books_collection.delete_one({'_id' : id_back})
             return {'message': 'Не удалось извлечь страницы из PDF', 'cod': '1006'}
-        
-    except PyMongoError as e:
-        """Ошибки pyMongo"""
-        if id_back is not None:
-            books_collection.delete_one({'_id' : id_back})
-        return {'message': f'Ошибка базы данных: {str(e)}', 'cod': '1004'}
     
     except Exception as r:
         """Любая ошибка"""
         if id_back is not None:
-            books_collection.delete_one({'_id' : id_back})
+            await books_collection.delete_one({'_id' : id_back})
         return {'message': f' Внутреняя Ошибка базы данных: {str(r)}', 'cod': '1005'}
     
     finally:
-        delete_file(metadata.book_name)
+        try:
+             await asyncio.get_running_loop().run_in_executor(
+                 None,
+                 delete_file,
+                 metadata.book_name
+                 )
+        except Exception as e:
+            print(f"Произошла ошибка при удалении временнего файла {e}")
     
 
 class BookDeleteModels(BaseModel):
@@ -65,9 +72,9 @@ class BookDeleteModels(BaseModel):
 async def delete_books(metadata: BookDeleteModels):
     """Функция для удаления страниц книги и """
     try:
-        delete_for_pages = books_pages.delete_many({'book_id' : ObjectId(metadata.id)})
+        delete_for_pages = await books_pages.delete_many({'book_id' : ObjectId(metadata.id)})
         if delete_for_pages.deleted_count == int(metadata.pages):
-            delete_books_count = books_collection.delete_one({'_id' : ObjectId(metadata.id)})
+            delete_books_count = await books_collection.delete_one({'_id' : ObjectId(metadata.id)})
             if delete_books_count.deleted_count == 1:
                 return {'message' : f'Книга удалена, удаленных страниц {delete_for_pages.deleted_count}', 'cod' : '2001'}
             return {
@@ -79,7 +86,7 @@ async def delete_books(metadata: BookDeleteModels):
             'cod' : '2002',
             }
     
-    except PyMongoError as e:
+    except Exception as e:
         """Ошибки pyMongo"""
         return {'message': f'Ошибка базы данных: {str(e)}', 'cod': '2004'}
     
@@ -90,7 +97,7 @@ class BookGetPage(BaseModel):
 @app.post('/get_page/')
 async def get_page(metadata: BookGetPage):
     try:
-        page = books_pages.find_one({'book_id' : ObjectId(metadata.book_id), 'page' : int(metadata.page)})
+        page = await books_pages.find_one({'book_id' : ObjectId(metadata.book_id), 'page' : int(metadata.page)})
         if page:
             return {
             'page_number' : page.get('page'),
@@ -98,7 +105,7 @@ async def get_page(metadata: BookGetPage):
             'content' : page.get('content'),
             }
         return {'message' : 'Страница не найдена', 'cod' : '3002'}
-    except PyMongoError as e:
+    except Exception as e:
         """Ошибки pyMongo"""
         return {'message': f'Ошибка базы данных: {str(e)}', 'cod': '3003'}
 
--- pdf.py
+++ pdf.py
@@ -1,14 +1,8 @@
-from bson import ObjectId
-import pymupdf
+import fitz
 import base64
 import os
 from pymongo import MongoClient
 
-
-def delete_file(remove_name):
-    """Удаление файла для очистки памяти"""
-    os.remove(f'{remove_name}.pdf')
-  
 
 def pdf_formater(book_content, book_name, book_id):
     """Берет данные закодированные в base 64, затем сохраняет его в пдф файл и с помощью
@@ -19,7 +13,7 @@ def pdf_formater(book_content, book_name, book_id):
         file.write(pdf_binary)
         file.flush()
            
-    doc = pymupdf.open(f'{book_name}.pdf')
+    doc = fitz.open(f'{book_name}.pdf')
     results = []
     count = 0
     for page in doc:
@@ -31,40 +25,10 @@ def pdf_formater(book_content, book_name, book_id):
             'page' : count,
             'content' : text,
         })
+    doc.close()
     return results
         
-
-
-   
-    
-"""Все связано тестами"""  
-def test_2():
-    uri = 'mongodb://root:example@localhost:27017/'
-    client = MongoClient(uri)
-    db = client['book_library']
-    books_collection = db.book
-    books_pages = db.pages
-    
-    data = {
-        'book_name' : 'Fillips',
-    }
-    id_back = books_collection.insert_one(data).inserted_id
-    if id_back:
-        first = code()
-        result = pdf_formater(first, 'Fillips', id_back)
-        books_pages.insert_many(result)
-    
-def test_3():
-    uri = 'mongodb://root:example@localhost:27017/'
-    client = MongoClient(uri)
-    db = client['book_library']
-    books_collection = db.book
-    books_pages = db.pages
-    
-    page = books_pages.find_one({'book_id' : ObjectId('68e3c51a36bfb0645555a2ed'), 'page' : 160})
-    return page
-if __name__ == '__main__':
-    test_2()
-    
-    
+def delete_file(remove_name):
+    """Удаление файла для очистки памяти"""
+    os.remove(f'{remove_name}.pdf')
     
